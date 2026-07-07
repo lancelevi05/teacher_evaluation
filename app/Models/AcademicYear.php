9aee5f4791e40cafdc1dadc2e0c7f118a62b36ef
lance Levi <lancelevi05@gmail.com>
2026-07-07 21:54:35 +0800

    added academic year

---
+++ app/Models/AcademicYear.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AcademicYear extends Model
+{
+    //
+
+    protected $fillable = [
+        'year_label',
+        'status',
+       
+    ];
+}
